97e64d7620706018434291df152be97660976c57
jmattaa <mattajonathan1@gmail.com>
2024-11-19 18:41:04 +0100

    add quote caching
    Signed-off-by: jmattaa <mattajonathan1@gmail.com>

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,43 @@
+<script>
+    import { onMount } from "svelte";
+
+    let quote = "";
+    const cacheKey = "tegquote:quote";
+    const cacheTimeKey = "tegquote:quoteTime";
+    const targetHourUTC = 17;
+
+    function shouldUpdateQuote() {
+        const now = new Date();
+        const utcHour = now.getUTCHours();
+        const cachedTime = localStorage.getItem(cacheTimeKey);
+        const cachedQuote = localStorage.getItem(cacheKey);
+
+        return (
+            utcHour === targetHourUTC ||
+            !cachedTime ||
+            !cachedQuote
+        );
+    }
+
+    async function loadText() {
+        const response = await fetch("./today.txt");
+        quote = await response.text();
+
+        localStorage.setItem(cacheKey, quote);
+        localStorage.setItem(cacheTimeKey, Date.now());
+    }
+
+    onMount(() => {
+        const cachedText = localStorage.getItem(cacheKey);
+
+        if (shouldUpdateQuote()) {
+            loadText();
+        } else {
+            quote = cachedText;
+        }
+    });
+</script>
+
 <main>
-    <h1 class="text-palette-2 text-6xl">en katt... e inte en hund</h1>
+    <h1 class="text-palette-2 text-6xl">{quote}</h1>
 </main>
