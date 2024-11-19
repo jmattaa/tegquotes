6c2b52abf111f7768d1cc9c230998868fa89ec1e
jmattaa <mattajonathan1@gmail.com>
2024-11-19 18:00:53 +0100

    initial commit
    en katt.. e inte en hund
    
    Signed-off-by: jmattaa <mattajonathan1@gmail.com>

---
+++ src/App.svelte
@@ -0,0 +1,3 @@
+<main>
+    <h1 class="text-palette-2 text-6xl">en katt... e inte en hund</h1>
+</main>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./index.html", './src/**/*.{svelte,js,ts}'],
+    theme: {
+        colors: {
+            "palette-1": "#6bc3a0",
+            "palette-2": "#099e63",
+            "palette2-1": "#ded890",
+            "palette2-2": "#b7aa1d",
+        },
+        extend: {},
+    },
+    plugins: [],
+}
